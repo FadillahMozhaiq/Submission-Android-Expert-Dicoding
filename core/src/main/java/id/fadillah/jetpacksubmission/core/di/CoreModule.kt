f48618f0171b2138ecaf4b383727bd80b69ac4c8
Fadillah Mozhaiq <mozhaiqramadhani05@gmail.com>
2021-06-25 15:35:23 +0800

    Add Obfuscation

--- core/src/main/java/id/fadillah/jetpacksubmission/core/di/CoreModule.kt
+++ core/src/main/java/id/fadillah/jetpacksubmission/core/di/CoreModule.kt
@@ -3,6 +3,7 @@ package id.fadillah.jetpacksubmission.core.di
 import androidx.room.Room
 import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.api.ChuckerInterceptor
+import id.fadillah.jetpacksubmission.core.BuildConfig
 import id.fadillah.jetpacksubmission.core.data.MovieRepository
 import id.fadillah.jetpacksubmission.core.data.source.local.LocalDataSource
 import id.fadillah.jetpacksubmission.core.data.source.local.room.MovieDatabase
@@ -10,6 +11,8 @@ import id.fadillah.jetpacksubmission.core.data.source.network.ApiService
 import id.fadillah.jetpacksubmission.core.data.source.network.RemoteDataSource
 import id.fadillah.jetpacksubmission.core.domain.repository.IMovieRepository
 import id.fadillah.jetpacksubmission.core.utils.helper.ConstantHelper.MAIN_URL
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -20,10 +23,15 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("movie_database_key".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java, "Movie.db"
-        ).fallbackToDestructiveMigration().build()
+        )
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 val networkModule = module {
@@ -37,6 +45,7 @@ val networkModule = module {
     }
     single {
         OkHttpClient.Builder()
+//            if(BuildConfig.DEBUG)
             .addInterceptor(get<ChuckerInterceptor>())
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
--- core/src/main/java/id/fadillah/jetpacksubmission/core/utils/helper/PreferenceHelper.kt
+++ core/src/main/java/id/fadillah/jetpacksubmission/core/utils/helper/PreferenceHelper.kt
@@ -1,14 +1,45 @@
 package id.fadillah.jetpacksubmission.core.utils.helper
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.securepreferences.SecurePreferences
 import id.fadillah.jetpacksubmission.core.R
 
 class PreferenceHelper(private val context: Context) {
-    private val preference = context.getSharedPreferences(
-        context.getString(R.string.preference_name),
-        Context.MODE_PRIVATE
-    )
+    private var preference: SharedPreferences =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val spec = KeyGenParameterSpec.Builder(
+                MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            )
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .build()
+            val masterKey = MasterKey.Builder(context)
+                .setKeyGenParameterSpec(spec)
+                .build()
+            EncryptedSharedPreferences
+                .create(
+                    context,
+                    context.getString(R.string.preference_name),
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                )
+        } else {
+            SecurePreferences(
+                context,
+                context.getString(R.string.preference_password),
+                context.getString(R.string.preference_name)
+            )
+        }
 
     fun setShowed() {
         preference.edit {
