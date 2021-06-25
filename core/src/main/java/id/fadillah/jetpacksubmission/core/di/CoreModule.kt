923f56a3721436191961fa041a39ebdd3a33b762
Fadillah Mozhaiq <mozhaiqramadhani05@gmail.com>
2021-06-25 15:59:45 +0800

    Add Certificate Pinning

--- core/src/main/java/id/fadillah/jetpacksubmission/core/di/CoreModule.kt
+++ core/src/main/java/id/fadillah/jetpacksubmission/core/di/CoreModule.kt
@@ -13,6 +13,7 @@ import id.fadillah.jetpacksubmission.core.domain.repository.IMovieRepository
 import id.fadillah.jetpacksubmission.core.utils.helper.ConstantHelper.MAIN_URL
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -44,11 +45,18 @@ val networkModule = module {
             .build()
     }
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
         OkHttpClient.Builder()
 //            if(BuildConfig.DEBUG)
             .addInterceptor(get<ChuckerInterceptor>())
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
